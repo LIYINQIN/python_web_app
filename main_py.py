85d9c64023f62f2ff2fed6f5674f1e159da77be0
DrIssah <52053029+DrIssah@users.noreply.github.com>
2023-10-16 21:21:29 +0300

    Update main_py.py
    added streamlt

--- main_py.py
+++ main_py.py
@@ -1,12 +1,10 @@
-
+import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
-
 import requests
 
-df  = pd.read_csv("balanced.csv")
-
+# Load the model and data
+df = pd.read_csv("balanced.csv")
 y = df['y']
 X = df.drop('y', axis=1)
 
@@ -15,31 +13,27 @@ from sklearn.ensemble import GradientBoostingRegressor
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3)
-
-# Train the model using your training data (X_train, y_train)
 model.fit(X_train, y_train)
 
-lat = float(input("Lat"))
-lon = float(input("Lon"))
+# Create a Streamlit app
+st.title("Flood Prediction App")
 
+# Collect user input
+lat = st.number_input("Enter Latitude")
+lon = st.number_input("Enter Longitude")
 
-
+# Make API request to get weather data
 url = "https://weatherapi-com.p.rapidapi.com/current.json"
-
 querystring = {"q": f"{lat},{lon}"}
 
 headers = {
-	"X-RapidAPI-Key": "fd686eb4fbmsh09a45c03eb9a89fp1a3435jsnce9ce4bbab3e",
-	"X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    "X-RapidAPI-Key": "fd686eb4fbmsh09a45c03eb9a89fp1a3435jsnce9ce4bbab3e",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
 }
 
 response = requests.get(url, headers=headers, params=querystring)
-
 main = response.json()['current']
 location = response.json()['location']
-
-
-
 
 Max_Temp = main['temp_f']
 Min_Temp = main['temp_c']
@@ -47,7 +41,7 @@ Rainfall = main['precip_mm'] * 100
 Relative_Humidity = main['humidity']
 Wind_Speed = main['wind_kph']
 Cloud_Coverage = main['cloud']
-LATITUDE = location['lat']	
+LATITUDE = location['lat']
 LONGITUDE = location['lon']
 
 data = {
@@ -63,21 +57,13 @@ data = {
 
 X_test = pd.DataFrame(data)
 
-
+# Make predictions
 predictions = model.predict(X_test)
-
 pr = predictions[0]
 
-threshold = 0.5  
-
-# Make the binary prediction
+# Display the result
+threshold = 0.5
 if pr >= threshold:
-    prediction = 1  # Flood
-    print("Flood ? Yes")
+    st.write("Flood Prediction: Yes")
 else:
-    prediction = 0  # No flood
-    print("Flood ? No")
-
-
-
-
+    st.write("Flood Prediction: No")
